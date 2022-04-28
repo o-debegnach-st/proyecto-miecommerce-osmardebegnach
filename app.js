6790189e7bbae7afa5498f8cb4ba4a5b9be26bda
Abuin <a446458@lbr000217.rio.ar.bsch>
2022-04-28 17:34:01 -0300

    agregue rutas al app.js

--- app.js
+++ app.js
@@ -4,8 +4,27 @@ const path = require('path');
 
 app.set('port', process.env.port || 3000) 
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('assets'));
+
 app.get('/', (req, res, next) =>{
-    res.send('<h1>Hello world<h1>');
+    res.render('pages/index');
+})
+app.get('/products', (req, res, next) =>{
+    res.render('pages/product');
+})
+app.get('/cart', (req, res, next) =>{
+    res.render('pages/cart');
+})
+app.get('/checkout', (req, res, next) =>{
+    res.render('pages/checkout');
+})
+app.get('/register', (req, res, next) =>{
+    res.render('pages/register');
+})
+app.get('/login', (req, res, next) =>{
+    res.render('pages/login');
 })
 
 app.listen(app.get('port'), server =>{
