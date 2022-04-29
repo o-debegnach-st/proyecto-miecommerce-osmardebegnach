81e40d2e5ce2830ffcbace35098ccea6b1a54043
Osmar <odebegnach@santandertecnologia.com>
2022-04-29 15:25:12 -0300

    Cambie la carta producto para que sea dinamica y el index para que la utilice

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+let whiskey = { 
+    urlImagen: 'images/whiskey.png',
+    precio: '19.900',
+nombre: 'Whiskey Jack Daniels Honey 750ml'
+}; 
+ let coca = { 
+    urlImagen: 'images/lata-coca-cola.png',
+    precio: '760',
+    nombre: 'Coca Cola Lata 220mL Pack Original x8'
+} 
+let generico = {
+    urlImagen: 'images/producto-generico.png',
+    precio: 'XX.XXX',
+    nombre: 'NOMBRE DEL PRODUCTO O SERVICO'
+} 
+
+let interes = [whiskey, generico, generico, generico]
+let masVendidos = [coca, generico, generico, generico, generico, generico, generico, generico]
+
 app.set('port', process.env.port || 3000) 
 
 app.set('view engine', 'ejs');
@@ -9,7 +28,9 @@ app.set('views', './views');
 app.use(express.static('assets'));
 
 app.get('/', (req, res, next) =>{
-    res.render('pages/index');
+    res.render('pages/index', {
+        interes, masVendidos
+    });
 })
 app.get('/products', (req, res, next) =>{
     res.render('pages/product');
