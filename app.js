cfe4b68c846af1c7a9e738db11994123bb83e142
Aquiles <aquiabuin@gmail.com>
2022-05-06 17:51:31 -0300

    agregue funcionalidad login

--- app.js
+++ app.js
@@ -5,17 +5,22 @@ const indexRouter = require("./src/routes/index")
 const productsRouter = require("./src/routes/products")
 const cartRouter = require("./src/routes/cart")
 const checkoutRouter = require("./src/routes/checkout")
+const usersRouter = require("./src/routes/users")
 
 app.set('port', process.env.port || 3000) 
 
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 app.use(express.static(path.resolve(__dirname,'./public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 
 app.use("/", indexRouter)
 app.use("/products", productsRouter)
 app.use("/cart", cartRouter)
 app.use("/checkout", checkoutRouter)
+
+app.use("/users", usersRouter)
 
 app.listen(app.get('port'), server =>{
     console.info(`Server listen on port ${app.get('port')}`);
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const {validationResult} = require('express-validator');
+const path = require('path');
+
 const register = (req, res, next) => {
     res.render('pages/register', {loggedIn: false})
 }
@@ -6,4 +10,23 @@ const login = (req, res, next) => {
     res.render('pages/login', {loggedIn: false})
 }
 
-module.exports = { register, login }
+const getUsers = () => JSON.parse(fs.readFileSync(path.resolve(__dirname,'../db/users.json'), "utf-8"))
+
+const registerPost = (req, res) => {
+    const errors = validationResult(req)
+    const users = getUsers()
+    if (errors.isEmpty()) {
+        users.push(req.body)
+        fs.writeFileSync(path.resolve(__dirname,'../db/users.json'), JSON.stringify(users))
+        res.render("pages/register", {
+            msg: "La cuenta se creó correctamente."
+        })
+    } else {
+        res.render('pages/register',{
+            errors: errors.errors,
+            old: req.body,
+        })
+    }
+}
+
+module.exports = { register, login, registerPost, getUsers }
