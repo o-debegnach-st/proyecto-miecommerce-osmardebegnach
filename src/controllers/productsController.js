781f867ff4b506e74da638b9358b23b84e65096e
Osmar <odebegnach@santandertecnologia.com>
2022-05-06 16:29:14 -0300

    Elimine variables sin uso

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,11 +1,7 @@
 const fetch = require("node-fetch");
 const apiBaseURL = "https://dhfakestore.herokuapp.com/api";
 
-let whiskey = {
-	urlImagen: "images/whiskey.png",
-	precio: "19.900",
-	nombre: "Whiskey Jack Daniels Honey 750ml",
-};
+
 let coca = {
 	urlImagen: "images/lata-coca-cola.png",
 	precio: "760",
@@ -16,18 +12,6 @@ let generico = {
 	precio: "XX.XXX",
 	nombre: "NOMBRE DEL PRODUCTO O SERVICIO",
 };
-
-let interes = [whiskey, generico, generico, generico];
-let masVendidos = [
-	coca,
-	generico,
-	generico,
-	generico,
-	generico,
-	generico,
-	generico,
-	generico,
-];
 const productos = [coca, generico, generico, generico];
 let loggedIn = false;
 
