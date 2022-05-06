d0699fea4c4c2f86be61d52fe8aed56bad93f37b
Rojas Lara <A446482@santandertecnologia.com.ar>
2022-05-06 16:42:54 -0300

    ultimos cambios

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,27 +1,5 @@
-<<<<<<< HEAD
-const fetch = require('node-fetch');
-
-const urlBase = 'https://dhfakestore.herokuapp.com/api/products'
-
-let whiskey = { 
-    urlImagen: 'images/whiskey.png',
-    precio: '19.900',
-    nombre: 'Whiskey Jack Daniels Honey 750ml'
-}; 
-let coca = { 
-    urlImagen: 'images/lata-coca-cola.png',
-    precio: '760',
-    nombre: 'Coca Cola Lata 220mL Pack Original x8'
-} 
-let generico = {
-    urlImagen: 'images/producto-generico.png',
-    precio: 'XX.XXX',
-    nombre: 'NOMBRE DEL PRODUCTO O SERVICIO'
-} 
-=======
 const fetch = require("node-fetch");
 const apiBaseURL = "https://dhfakestore.herokuapp.com/api";
->>>>>>> 0f39c37f1b5dcafed0ea87b3222369ce01596a3c
 
 
 let coca = {
@@ -80,20 +58,11 @@ const root = (req, res) => {
 };
 
 const products = (req, res, next) => {
-<<<<<<< HEAD
-    fetch(`${urlBase}`).then(res=>res.json()).then(json=>res.render('pages/products',{products:json,loggedIn}))
-  /*   res.render('pages/products',{
-        products:productos,
-        loggedIn
-    }); */
-}
-=======
 	res.render("pages/products", {
 		products: productos,
 		loggedIn,
 	});
 };
->>>>>>> 0f39c37f1b5dcafed0ea87b3222369ce01596a3c
 
 const cart = (req, res, next) => {
 	res.render("pages/cart", { loggedIn });
@@ -103,15 +72,4 @@ const checkout = (req, res, next) => {
 	res.render("pages/checkout", { loggedIn: false });
 };
 
-<<<<<<< HEAD
-const productsRelated = (req,res,next)=>{
-    const { id } = req.params;
-    console.log(id);
-    fetch(`${urlBase}`).then(res=>res.json()).then(json=>console.log(json))
-   //res.render('pages/products',{loggedIn:false})
-}
-
-module.exports = {root, products, cart, checkout, productsRelated}
-=======
 module.exports = { root, products, suggestedProducts, mostwantedProducts, cart, checkout };
->>>>>>> 0f39c37f1b5dcafed0ea87b3222369ce01596a3c
