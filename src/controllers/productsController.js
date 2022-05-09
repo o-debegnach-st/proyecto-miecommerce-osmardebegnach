520e862dfa4eec710c20f939bdcb621601a7aa94
Rojas Lara <A446482@santandertecnologia.com.ar>
2022-05-09 11:38:09 -0300

    pendientes

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,18 +1,8 @@
 const fetch = require("node-fetch");
 const apiBaseURL = "https://dhfakestore.herokuapp.com/api";
+const BaseURL = " https://fakestoreapi.com/"
 
 
-let coca = {
-	urlImagen: "images/lata-coca-cola.png",
-	precio: "760",
-	nombre: "Coca Cola Lata 220mL Pack Original x8",
-};
-let generico = {
-	urlImagen: "images/producto-generico.png",
-	precio: "XX.XXX",
-	nombre: "NOMBRE DEL PRODUCTO O SERVICIO",
-};
-const productos = [coca, generico, generico, generico];
 let loggedIn = false;
 
 const getSuggestedProducts = async () => {
@@ -24,7 +14,15 @@ const getSuggestedProducts = async () => {
 		console.log(error);
 	}
 };
-
+const getAll = async()=>{
+	try{
+		let response = await fetch(`${apiBaseURL}/products`)
+		let data = await response.json();
+		return data;
+	}catch(error){
+		console.log(error)
+	}
+}
 const getMostwantedProducts = async () => {
     try {
         let response = await fetch(`${apiBaseURL}/products/mostwanted`);
@@ -34,13 +32,25 @@ const getMostwantedProducts = async () => {
 		console.log(error);
 	}
 };
-
+const getProductById = async (id)=>{
+	try{
+		let response = await fetch(`${apiBaseURL}/products/${id}`);
+		let data = await response.json();
+		return data;
+	} catch(error){
+		console.log(error);
+	}
+}
 const suggestedProducts = (req, res) => {
     getSuggestedProducts().then(data => res.json(data.slice(0, 5)))
 }
 const mostwantedProducts = (req, res) => {
     getMostwantedProducts().then(data => res.json(data.slice(0, 10)))
 }
+
+/* const productById = (req,res)=> {
+	getProductById().then(res=>res.json());
+} */
 
 const root = (req, res) => {
 	Promise.all([getSuggestedProducts(), getMostwantedProducts()]).then((values) => {
@@ -58,11 +68,43 @@ const root = (req, res) => {
 };
 
 const products = (req, res, next) => {
-	res.render("pages/products", {
-		products: productos,
-		loggedIn,
+	let {id}=req.params;
+	Promise.all([getAll(),getProductById(id)]).then((values) => {
+		let productId = values[1]
+		let all = values[0].filter(product=>productId.category===product.category)
+		if (req.query.email !== undefined) {
+			loggedIn = true;
+		}
+		res.render("pages/products", {
+			interes: all.slice(0, 5),
+			product:productId,
+			loggedIn,
+		});
 	});
 };
+
+
+const getAllProducts = async(req,res)=>{
+	try{
+		let allProducts = await getAll();
+		res.send(allProducts)
+	}catch(error){
+		console.log(error)
+	}
+};
+
+const productDetail =async (req,res)=>{
+	let {id}=req.params;
+	try{
+		let productId = await getProductById(id);
+		let allProducts = await getAll();
+		let results = allProducts.filter(product=>productId.category===product.category)
+		res.send(results)
+	}catch(err){
+		console.log(err)
+	}
+
+}
 
 const cart = (req, res, next) => {
 	res.render("pages/cart", { loggedIn });
@@ -72,4 +114,4 @@ const checkout = (req, res, next) => {
 	res.render("pages/checkout", { loggedIn: false });
 };
 
-module.exports = { root, products, suggestedProducts, mostwantedProducts, cart, checkout };
+module.exports = { root, products, suggestedProducts, mostwantedProducts, cart, checkout,getAllProducts, productDetail };
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,10 +1,11 @@
-const { products, suggestedProducts, mostwantedProducts } = require("../controllers/productsController");
+const { products, suggestedProducts, mostwantedProducts, getAllProducts , productDetail} = require("../controllers/productsController");
 const router = require("express").Router()
 
 let loggedIn = false;
 
-router.get("/", products)
-router.get('/:id', productsRelated )
+router.get("/:id", products)
+router.get('/:id/product-detail', productDetail)
+router.get('/', getAllProducts)
 
 router.get('/suggested', suggestedProducts)
 
