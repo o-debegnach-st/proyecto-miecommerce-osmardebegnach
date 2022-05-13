92475aeee6fcebc8445eb806696dc5b8f80267c3
Rojas Lara <A446482@santandertecnologia.com.ar>
2022-05-13 17:03:27 -0300

    blur cierre menus

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,16 +1,17 @@
 const button = document.querySelector('.header-user-card');
 const userMenu = document.querySelector('#userMenu')
 const body = document.querySelector('body')
+const popUp = document.querySelector('#fondoBlur')
 
 button.addEventListener('click',(e)=>{
     e.stopPropagation();
+    popUp.classList.toggle('hidden')
     userMenu.classList.toggle('open')
-
 })
 
-body.addEventListener('click',(e)=>{
-    console.log('click')
+popUp.addEventListener('click',()=>{
     if (userMenu.classList.contains('open')){
-        userMenu.classList.remove('open')
+        userMenu.classList.toggle('open')
+        popUp.classList.toggle('hidden')
     }
 })
