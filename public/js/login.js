422cdee70854122771d86ed0198e338c124458a1
Osmar <odebegnach@santandertecnologia.com>
2022-05-16 17:05:26 -0300

    Agregue funcionalidad en la vista login para que recuerde el nombre del usuario

---
+++ public/js/login.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', (event) => {
+  let form = document.querySelector('form')
+  let check = document.querySelector('form #recuerdame')
+  let userName = document.querySelector('#email')
+ 
+  if(localStorage.userName){
+    userName.value = localStorage.userName
+    check.checked = true
+  }
+
+  form.addEventListener('submit', ()=>{
+    let recordar = check.checked
+    if(recordar){
+      localStorage.setItem('userName', userName.value)
+      console.log('recordado');
+    }
+  })
+});
