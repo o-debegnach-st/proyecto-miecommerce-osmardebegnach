7ff988600665c85f865de1eecffab1074aba1014
Aquiles <aquiabuin@gmail.com>
2022-05-16 19:49:53 -0300

    agregue colores para el darkmode

--- public/js/mode.js
+++ public/js/mode.js
@@ -1,5 +1,20 @@
 const changeMode = document.querySelector('#changeMode')
+const userMenuTexto = document.querySelector('.user-menu-tema-texto')
+
+if (localStorage.darkMode) {
+    body.classList.toggle("darkMode")
+    userMenuTexto.innerText = "Claro"
+} else {
+    userMenuTexto.innerText = "Oscuro"
+}
 
 changeMode.addEventListener('click',()=>{
     body.classList.toggle("darkMode")
+    if (localStorage.darkMode) {
+        localStorage.removeItem("darkMode")
+        userMenuTexto.innerText = "Oscuro"
+    } else {
+        localStorage.setItem("darkMode", "true")
+        userMenuTexto.innerText = "Claro"
+    }
 })
